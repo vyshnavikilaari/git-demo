e403eac4ff0332c2f0499021cdbb7984801fec6b
vyshnavikilaari <kilaarivyshnavi@gmail.com>
2024-10-28 16:45:15 +0530

    Day-7 Express

---
+++ Express/sample-server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const Port = 3000
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.status(200).json("Hello");
+    
+})
+
+app.listen(Port,()=>{
+    // console.log("Server is running in Port :"+Port)
+    console.log(`Server is running in Port :${Port}`) 
+ 
+})
